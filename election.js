f2166f0836bc345f18c3bb2fec3937e74ace4fd2
Daniel Ang <dan.justin.ang@gmail.com>
2018-11-01 16:06:22 -0400

    [Election] Added AJAX and displayed the name and votes of each character

--- election.js
+++ election.js
@@ -1,5 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
+var select = document.querySelector('#select');
 
-  // Imagination!
+  axios({
+   url: 'https://bb-election-api.herokuapp.com/',
+   method: 'get',
+   responseType: 'text'
+}).then(function(response){
+  console.log(response.data.candidates);
+  response.data.candidates.forEach(function(candidate){
+    var addPeople = document.createElement('li');
+    addPeople.innerHTML = `${candidate.name} ${candidate.votes}`
+    select.appendChild(addPeople);
+
+  })
+})
+
 
 });
