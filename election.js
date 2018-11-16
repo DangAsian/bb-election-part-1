9e6fb53614dad397afca6cff063da90da6eed1dd
Daniel Ang <dan.justin.ang@gmail.com>
2018-11-16 12:08:38 -0500

    added synchronous calls

--- election.js
+++ election.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
 var select = document.querySelector('#select');
+// var button = document.querySelector('button');
 
   axios({
    url: 'https://bb-election-api.herokuapp.com/',
@@ -9,11 +10,40 @@ var select = document.querySelector('#select');
   console.log(response.data.candidates);
   response.data.candidates.forEach(function(candidate){
     var addPeople = document.createElement('li');
+    var formCreate = document.createElement('form');
+    var submit = document.createElement('input');
+    var hidden = document.createElement('input');
+    submit.setAttribute('type', 'submit');
+    formCreate.setAttribute('action', "https://bb-election-api.herokuapp.com/vote");
+    formCreate.setAttribute('method', "post");
+    hidden.setAttribute('type', 'hidden');
+    hidden.setAttribute('name', "id");
+    hidden.setAttribute('value', candidate.id);
+    submit.innerHTML = "vote";
     addPeople.innerHTML = `${candidate.name} ${candidate.votes}`
+    formCreate.innerHTML = "I'm a form"
     select.appendChild(addPeople);
+    addPeople.appendChild(formCreate);
+    formCreate.appendChild(hidden);
+    formCreate.appendChild(submit);
 
+    // formCreate.addEventListener("submit", function(e){
+    //   e.preventDefault();
+    //   axios({
+    //    url: 'https://bb-election-api.herokuapp.com/vote',
+    //    method: 'post',
+    //    data: {name:}
+    // }).then(function(response){
+    //     console.log(response.data);
+    //
+    // })
+    // })
   })
 })
+
+// button.addEventListener("click", function(){
+//   console.log("ih");
+// });
 
 
 });
